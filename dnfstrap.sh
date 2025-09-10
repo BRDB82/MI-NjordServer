59ca2b8b6dd15aa25cca3f149f7ed13edfd08f55
BRDB82 <b.r.debrouwer@gmail.com>
2025-09-11 00:28:28 +0200

    Update dnfstrap.sh

--- dnfstrap.sh
+++ dnfstrap.sh
@@ -10,19 +10,26 @@
 shopt -s extglob
 
 hostcache=0
-copykeyring=1
-copymirrorlist=1
+dnf_args=()
+dnfmode="install"
+unshare=0
+copyconf=0
+dnf_config="/etc/dnf/dnf.conf"
+
+m4_include(common)
 
 usage() {
   cat <<EOF
 usage: ${0##*/} [options] root [packages...]
 
   Options:
-    -C config      Use an alternate config file for pacman
+    -C <config>    Use an alternate config file for dnf
     -c             Use the package cache on the host, rather than the target
-    -G             Avoid copying the host's dnf keyring to the target
     -i             Prompt for package confirmation when needed (run interactively)
-    -M             Avoid copying the host's repo ist to the target
+    -M             Avoid copying the host's repo files to the target
+    -N             Run in unshare mode as a regular user
+    -P             Copy the host's dnf config to the target
+    -U             Use dnf install from local RPMs
 
     -h             Print this help message
 
@@ -38,3 +45,12 @@ if [[ -z $1 || $1 = @(-h|--help) ]]; then
 fi
 
 (( EUID == 0 )) || die 'This script must be run with root privileges'
+
+# creat obligatory directories
+msg 'Creating install at %s' "$newroot"
+# shellcheck disable=SC2174 # permissions are perfectly fine here
+mkdir -m 0755 -p "$newroot"/var/{cache/dnf,lib,log} "$newroot"/{dev,run,etc/yum.repos.d}
+# shellcheck disable=SC2174 # permissions are perfectly fine here
+mkdir -m 1777 -p "$newroot"/tmp
+# shellcheck disable=SC2174 # permissions are perfectly fine here
+mkdir -m 0555 -p "$newroot"/{sys,proc}
