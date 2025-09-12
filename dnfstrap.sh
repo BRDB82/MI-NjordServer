c50d144ade03174fee7af43ad1fe0561f923c256
BRDB82 <b.r.debrouwer@gmail.com>
2025-09-12 13:56:23 +0200

    Update dnfstrap.sh

--- dnfstrap.sh
+++ dnfstrap.sh
@@ -10,13 +10,11 @@
 shopt -s extglob
 
 hostcache=0
+copyrepolist=1
 dnf_args=()
 dnfmode="install"
-unshare=0
 copyconf=0
 dnf_config="/etc/dnf/dnf.conf"
-
-m4_include(common)
 
 usage() {
   cat <<EOF
@@ -25,31 +23,31 @@ usage: ${0##*/} [options] root [packages...]
   Options:
     -C <config>    Use an alternate config file for dnf
     -c             Use the package cache on the host, rather than the target
+    -D             Skip dnf dependency checks
     -i             Prompt for package confirmation when needed (run interactively)
-    -M             Avoid copying the host's repo files to the target
+    -M             Avoid copying the host's repolist to the target
     -P             Copy the host's dnf config to the target
-    -U             Use dnf install from local RPMs
 
     -h             Print this help message
 
-dnfstrap installs packages to the specified new root directory. If no packages
-are given, dnfstrap defaults to the "base" group.
+pacstrap installs packages to the specified new root directory. If no packages
+are given, pacstrap defaults to the "base" group.
 
 EOF
 }
 
 dnfstrap() {
   (( EUID == 0 )) || die 'This script must be run with root privileges'
-  
-  # creat obligatory directories
-  msg 'Creating install at %s' "$newroot"
+
+  # create obligatory directories
+  msg 'Creating install root at %s' "$newroot"
   # shellcheck disable=SC2174 # permissions are perfectly fine here
-  mkdir -m 0755 -p "$newroot"/var/{cache/dnf,lib,log} "$newroot"/{dev,run,etc/yum.repos.d}
+  mkdir -m 0755 -p "$newroot"/var/{cache/dnf,lib/rpm,log} "$newroot"/{dev,run,etc/yum.repos.d}
   # shellcheck disable=SC2174 # permissions are perfectly fine here
   mkdir -m 1777 -p "$newroot"/tmp
   # shellcheck disable=SC2174 # permissions are perfectly fine here
   mkdir -m 0555 -p "$newroot"/{sys,proc}
-  
+
   # mount API filesystems
   $setup "$newroot" || die "failed to setup chroot %s" "$newroot"
 
@@ -58,8 +56,71 @@ dnfstrap() {
     die 'Failed to install packages to new root'
   fi
 
+  if (( copyrepolist )); then
+    # install the host's repo definitions onto the new root
+    cp -a /etc/yum.repos.d "$newroot/etc/"
+  fi
+
   if (( copyconf )); then
     cp -a "$dnf_config" "$newroot/etc/dnf/dnf.conf"
   fi
 }
 
+if [[ -z $1 || $1 = @(-h|--help) ]]; then
+  usage
+  exit $(( $# ? 0 : 1 ))
+fi
+
+while getopts ':C:cDiMPh' flag; do
+  case $flag in
+    C)
+      dnf_config=$OPTARG
+      ;;
+    D)
+      dnf_args+=(--setopt=skip_if_unavailable=True)
+      ;;
+    c)
+      hostcache=1
+      ;;
+    i)
+      dnf_args+=("--assumeyes")
+      ;;
+    M)
+      copyrepolist=0
+      ;;
+    P)
+      copyconf=1
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    :)
+      die '%s: option requires an argument -- '\''%s'\'' "${0##*/}" "$OPTARG"
+      ;;
+    ?)
+      die '%s: invalid option -- '\''%s'\'' "${0##*/}" "$OPTARG"
+      ;;
+  esac
+done
+
+shift $(( OPTIND - 1 ))
+
+(( $# )) || die "No root directory specified"
+newroot=$1
+shift
+
+[[ -d $newroot ]] || die "%s is not a directory" "$newroot"
+
+dnf_args+=("${@:-@core}" --config="$dnf_config" --installroot="$newroot")
+
+if (( ! hostcache )); then
+  dnf_args+=(--setopt=keepcache=False)
+fi
+
+if (( ! interactive )); then
+  dnf_args+=(--assumeyes)
+fi
+
+setup=chroot_setup
+dnfstrap
