4009eea824a6e6dc90990577ddec1ab6c0079715
BRDB82 <b.r.debrouwer@gmail.com>
2025-09-13 22:09:17 +0200

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -427,8 +427,8 @@ sgdisk -Z "${DISK}" # zap all on disk
 sgdisk -a 2048 -o "${DISK}" # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' "${DISK}" # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+1GiB --typecode=2:ef00 --change-name=2:'EFIBOOT' "${DISK}" # partition 2 (UEFI Boot Partition)
+sgdisk -n 1::+1G --typecode=1:8300 --change-name=1:'BOOT' "${DISK}" # partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+1G --typecode=2:ef00 --change-name=2:'EFIBOOT' "${DISK}" # partition 2 (UEFI Boot Partition)
 sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' "${DISK}" # partition 3 (Root), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 "${DISK}"
@@ -442,24 +442,28 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 if [[ "${DISK}" =~ "nvme" ]]; then
+    partition1=${DISK}p1
     partition2=${DISK}p2
     partition3=${DISK}p3
 else
+    partition1=${DISK}1
     partition2=${DISK}2
     partition3=${DISK}3
 fi
 
-if [[ "${FS}" == "xfs" ]]; then
-    mkfs.fat -F32 -n "EFIBOOT" "${partition2}"
+mkfs.ext4 -L BOOT "${partition1}"
+mkfs.fat -F32 -n "EFIBOOT" "${partition2}"
+
+if [[ "${FS}" == "xfs" ]]; then 
     mkfs.xfs -f -L ROOT "${partition3}"
     mount -t xfs "${partition3}" /mnt
 elif [[ "${FS}" == "ext4" ]]; then
-    mkfs.fat -F32 -n "EFIBOOT" "${partition2}"
     mkfs.ext4 "${partition3}"
     mount -t ext4 "${partition3}" /mnt
 fi
 
-BOOT_UUID=$(blkid -s UUID -o value "${partition2}")
+BOOT_UUID=$(blkid -s UUID -o value "${partition1}")
+EFI_UID=$(blkid -s UUID -o value "${partition2}")
 
 sync
 if ! mountpoint -q /mnt; then
@@ -467,8 +471,9 @@ if ! mountpoint -q /mnt; then
     exit 1
 fi
 
-mkdir -p /mnt/boot
+mkdir -p /mnt/boot/efi
 mount -U "${BOOT_UUID}" /mnt/boot/
+mount -U "${EFI_UUID}" /mnt/boot/efi
 
 if ! grep -qs '/mnt' /proc/mounts; then
     echo "Drive is not mounted, cannot continue"
