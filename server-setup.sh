cc5bdcfe4275daead52a61bc0a52162a0cd3b037
BRDB82 <b.r.debrouwer@gmail.com>
2025-09-17 00:07:00 +0200

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -505,6 +505,8 @@ echo -ne "
 # Detect EFI and install base system
 mkdir -p /mnt/etc/dnf/vars
 echo "$VERSION" > "/mnt/etc/dnf/vars/releasever"
+echo "x86_64" > "/mnt/etc/dnf/vars/baseearch"
+echo "rocky" > "/mnt/etc/dnf/vars/rltyp"
 if [[ ! -d "/sys/firmware/efi" ]]; then
     dnfstrap /mnt @core @development-tools kernel linux-firmware grub2 efibootmgr grub2-efi-x64 grub2-efi-x64-modules --assumeyes
 else
